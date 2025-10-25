d59627e5f30c54840a3e547728a7cb35cff7b662
Remone Teleporta <remoneteleporta@gmail.com>
2025-10-25 22:19:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { getDatabase,
          remove } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js"
 
 const firebaseConfig = {
-    databaseURL: "https://leads-tracker-app-e1436-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: process.env.DATABASE_URL
 }
 
 const app = initializeApp(firebaseConfig)
